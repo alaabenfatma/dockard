18ee372f42c71b4e18f2ec8c0614a39b56879409
Alaa Ben Fatma <9027148+alaabenfatma@users.noreply.github.com>
2022-09-10 19:56:11 +0200

    Put the main page only on app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import { SplitPane } from "react-collapse-pane";
 
-import TextEditor from './components/TextEditor';
-import OverviewFlow from './components/OverviewFlow';
+
+import Main from './pages/main';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <SplitPane split="vertical" collapse={true}>
-          <div >
-            <TextEditor />
-          </div>
-          <div>
-            <OverviewFlow />
-          </div>
-        </SplitPane>
-
+        <Main />
       </header>
     </div>
   );
